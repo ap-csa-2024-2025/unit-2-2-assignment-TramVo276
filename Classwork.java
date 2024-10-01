d54f104e4169eedafab11f1964999495d82e641f
TramVo276 <tvo941352@gmail.com>
2024-10-01 10:17:30 -0700

    jalsdfk

--- Classwork.java
+++ Classwork.java
@@ -1,7 +1,21 @@
+import java.util.Scanner;
 public class Classwork
 {
   public static void main(String[] args)
   {
-    // write your code
+    System.out.println("Hello");
+    Scanner sc = new Scanner(System.in);
+    System.out.println("what type pf item are you buying?");
+    String item;
+    item = sc.nextLine();
+    System.out.println("How many are you buying?");
+    int number;
+    number = sc.nextInt();
+    System.out.println("How much does each one weight?");
+    double weight;
+    weight = sc.nextDouble();
+    System.out.println(number + item + " at " + weight + " pounds each will weigh " + weight * item + "pounds total");
+
   }
 }
+     
